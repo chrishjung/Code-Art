ecaf1d959ada6d3fb28bf5813640822db369af30
chrishjung <chrishjung@gmail.com>
2023-03-24 12:14:14 -0700

    naomi

--- Mariano/sketch.js
+++ Mariano/sketch.js
@@ -1,28 +1,46 @@
+//variables for each image
+let img1;
+let img2;
+let BG;
 
-let img;
-
-function preload(){
- img = loadImage('cat.jpg');
+function preload() {
+ img2 = loadImage('frame.png');
+ img1 = loadImage('woodblock.png');
 }
-
-
 function setup() {
-  createCanvas(400, 400);
- 
+  createCanvas(1600, 1017);
 }
 
 function draw() {
-  background(220);
-cat(mouseX, 0, 200, 200, 10);
-cat(200, mouseY, 200, 200, 5);
-cat(200, 0, 200, 200, 20)
-cat(0, 200, 200, 200, 2)
+  //loading all three images
+  clear()
+  woodblock(300, 0, 300, 300, 10);
+  woodblock(650, 0, 300, 300, 5);
+  woodblock(1000, 0, 300, 300, 5);
+  woodblock(300, 350, 300, 300, 10);
+  woodblock(650, 350, 300, 300, 5);
+  woodblock(1000, 350, 300, 300, 5);
+  woodblock(300, 700, 300, 300, 10);
+  woodblock(650, 700, 300, 300, 5);
+  woodblock(1000, 700, 300, 300, 5);
+  image(img2, 0, 0, width, height)
+  
 }
 
 
-function cat(tx, ty, tw, th, tsw){
-image(img, tx,ty, tw, th)
-filter(GRAY);
-strokeWeight(tsw);
-line(tx, ty, (tw+tx), (th+ty));
+function frame(){
+image(img2, 0, 0)
 }
+
+function woodblock(tx, ty, tw, th){
+ image(img1, tx, ty, tw, th) 
+}
+
+//this is how to make any function
+
+// you can make a function out of everything, for example; if we remove the loading
+
+// image into this function, of course it doesn't work anymore. But if you add
+// 'cat();' into any other function below. It works, not only that, that applies
+// to filter, stroke, and line. Not only that but you can multiply that
+
--- Naomi/sketch.js
+++ Naomi/sketch.js
@@ -1,28 +1,48 @@
+let capture
+let imgBorder1
+let imgBorder2
+let alpha1
+let alpha2
 
-let img;
-
-function preload(){
- img = loadImage('cat.jpg');
-}
-
+function windowResized() {
+  resizeCanvas(windowWidth*0.7, windowHeight*0.7) 
+ }
 
 function setup() {
-  createCanvas(400, 400);
- 
+  createCanvas(windowWidth*0.7, windowHeight*0.7)
+  capture = createCapture(VIDEO)
+  capture.hide()
+}
+
+function preload(){
+  imgBorder1 = loadImage('image/imgborder1.png')
+  imgBorder2 = loadImage('image/imgborder2.png')
 }
 
 function draw() {
-  background(220);
-cat(mouseX, 0, 200, 200, 10);
-cat(200, mouseY, 200, 200, 5);
-cat(200, 0, 200, 200, 20)
-cat(0, 200, 200, 200, 2)
+  background(255, 255, 255);
+  image(capture, 10, 20, height*0.95*1.25, height*0.95)
+  value = imgBorder1
+  tint (255, alpha2)
+  image (imgBorder2, 0, 0, width, height)
+  tint(255, alpha1)
+  image(imgBorder1, 0, 0, width, height)
+}
+  
+
+function keyPressed(){
+  if (keyCode === 32 ) {
+    if (alpha2==0){
+      alpha2=255
+      alpha1=0
+    }
+    else
+    alpha2=0
+    alpha1=255
+    }
+  } 
+  if (key === 'f') {
+let fs = fullscreen();
+fullscreen(!fs) 
 }
 
-
-function cat(tx, ty, tw, th, tsw){
-image(img, tx,ty, tw, th)
-filter(GRAY);
-strokeWeight(tsw);
-line(tx, ty, (tw+tx), (th+ty));
-}
